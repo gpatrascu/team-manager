59f27a7cafdaeb1108970624acf0b71f68d8f6fe
g.patrascu <g.patrascu@criteo.com>
2025-12-26 10:30:17 +0200

    Fix: Allow anonymous access to app routes

---
+++ setVariables.sh
@@ -0,0 +1,4 @@
+az staticwebapp appsettings set \
+       --name TeamManager \
+       --resource-group rg-team-manager \
+       --setting-names FACEBOOK_APP_ID=1599199241087510 FACEBOOK_APP_SECRET=1b3cad25ffebdfe43a1ac04de776682e
