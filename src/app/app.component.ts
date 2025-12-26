ba5fbb4321f8cd964e20d04d2ff98335d852602b
g.patrascu <g.patrascu@criteo.com>
2025-12-26 10:06:43 +0200

    Add Facebook authentication, privacy policy, and data deletion pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,19 @@ import { CommonModule } from '@angular/common';
 export class AppComponent implements OnInit {
   title = 'Team Manager';
   message = '';
+  userInfo: any = null;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get('/api/GetMessage', { responseType: 'text' })
       .subscribe(data => this.message = data);
+    
+    this.http.get('/.auth/me')
+      .subscribe(data => this.userInfo = data);
+  }
+
+  logout() {
+    window.location.href = '/.auth/logout';
   }
 }
